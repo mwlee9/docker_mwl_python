6461888cf6acb0264fa50b6b71b3a03b0d3b3403
mwlee9 <mwlee9@gmail.com>
2021-12-21 10:38:37 -0600

    Update python vs 3.10.1 and .devcontainer

--- 2stop_remove_run_mount.bat
+++ 2stop_remove_run_mount.bat
@@ -1,14 +1,10 @@
-docker stop pyspark
+docker stop python
 
-docker rm pyspark
+docker rm python
 
-docker run -dp 8889:8888 -p 4040:4040 ^
-  -p 37899:37899 ^
-  -p 5050:5050 ^
-  -p 33139-33155:33139-33155 ^
-  -p 45029-45045:45029-45045 ^
+docker run -dp 8888:8888 ^
 -it --mount type=bind,source=%cd%\VOLUMES,target=/home/volumes ^
---name pyspark ^
-lee/pyspark ^
-jupyter notebook --ip=0.0.0.0 --port=8888 --NotebookApp.token='' --allow-root
+--name python ^
+lee/python ^
+jupyter lab --ip=0.0.0.0 --port=8888 --NotebookApp.token='' --allow-root
 pause
